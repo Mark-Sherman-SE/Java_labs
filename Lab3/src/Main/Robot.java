1afb6c6fac8b9707d5b46df58d5517ab81e845bd
Sherman_Mark <marshann2012@gmail.com>
2020-04-19 15:46:57 +0300

    Adjusting the solution

--- Lab3/src/Main/Robot.java
+++ Lab3/src/Main/Robot.java
@@ -1,10 +1,12 @@
 package Main;
 
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.Phaser;
 
 public class Robot extends Thread {
+    private static final Phaser PHASER = new Phaser(3);
     private final String subject;
-    private Student student;
+    private Student student = null;
     public BlockingDeque<Student> students;
 
     public Robot(BlockingDeque<Student> students, String subject) {
@@ -17,10 +19,12 @@ public class Robot extends Thread {
     public void run() {
         try {
             while (true) {
+                System.out.println("Robot " + subject);
                 student = students.peek();
                 if (student != null) {
                     if (student.getSubject().equals(subject)) {
                         student = students.take();
+                        PHASER.arrive();
                         System.out.println(subject + " teacher started verifying");
                         while (student.getLabsCount() != 0) {
                             System.out.println("Robot " + subject + " is working, " + student.getLabsCount() + " left, student number " + student.getNumberOfStudent());
@@ -28,9 +32,11 @@ public class Robot extends Thread {
                         }
                         System.out.println(subject + " teacher finished verifying");
                     } else if (student.getSubject().equals("finish")) {
+                        PHASER.arriveAndDeregister();
                         break;
                     }
                 }
+                PHASER.arriveAndAwaitAdvance();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
